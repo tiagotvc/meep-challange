74a41150a6343a2410a196e49c19afb420481fc5
Tiago Carvalho <carvalho110591@gmail.com>
2023-01-31 10:04:36 -0600

    [meep] - Limpado código da página inicial - Tiago Carvalho

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import Layout from "@/components/Layout/Layout";
+import { SectionBackground } from "@/components/SectionBackground";
+import Link from "next/link";
+import { CompletePokemon, usePokemon } from "@/store/index";
+import { createProkemon } from "utils/functions";
+import { ItemCard } from "@/components/ItemCard";
+import { Grid } from "@/styles/index-styles";
+export { getServerSideProps } from "@/store/index";
 
-export default function Home() {
+const Catalog = () => {
+  const { pokemon } = usePokemon();
+  const parsedPokemon = createProkemon(pokemon);
+
   return (
-    <Layout>
-      <div className="home_page">Essa é a Página Inicial</div>
-    </Layout>
+    <SectionBackground background={false}>
+      <Grid>
+        {parsedPokemon.map((item: CompletePokemon) => (
+          <Link key={`${item.id}`} href={`/${item.id}`}>
+            <ItemCard rarity={item.rarity} item={item} />
+          </Link>
+        ))}
+      </Grid>
+    </SectionBackground>
   );
-}
+};
+
+export default Catalog;
