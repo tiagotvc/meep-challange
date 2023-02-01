49f61967910aeff9ec119d9488548b08a9a29315
Tiago Carvalho <carvalho110591@gmail.com>
2023-01-31 20:34:41 -0600

    [meep] - Excluido alguns componentes descontinuados - Tiago Carvalho

--- src/components/TextComponent/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./styles";
-
-export type TextComponentProps = {
-  children: string;
-};
-
-export const TextComponent = ({ children }: TextComponentProps) => {
-  return <Container dangerouslySetInnerHTML={{ __html: children }} />;
-};
--- src/components/TextComponent/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled, { css } from "styled-components";
-
-export const Container = styled.div`
-  ${({ theme }) => css`
-    font-size: ${theme.font.sizes.medium};
-  `}
-`;
